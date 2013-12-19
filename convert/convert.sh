69a1f1e2647433f13be1cdd008e699ac870ac1f7
CornGuo <cornguo@msn.com>
2013-12-19 19:51:06 +0800

    use lower bitrate

--- convert/convert.sh
+++ convert/convert.sh
@@ -3,6 +3,6 @@ rm -f *.mp3 *.ogg
 for file in `ls ../clips/*.ogg`
 do
     fname=`basename $file | cut -d '.' -f1`
-    ffmpeg -loglevel panic -i ../clips/$fname.ogg -acodec libvorbis -ab 48k $fname.ogg
-    ffmpeg -loglevel panic -i ../clips/$fname.ogg -acodec libmp3lame -ab 48k $fname.mp3
+    ffmpeg -loglevel panic -i ../clips/$fname.ogg -acodec libvorbis -ac 1 -ab 32k $fname.ogg
+    ffmpeg -loglevel panic -i ../clips/$fname.ogg -acodec libmp3lame -ac 1 -ab 32k $fname.mp3
 done
