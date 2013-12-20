181e2f274b61acadedcdc6532aa48297c3864192
CornGuo <cornguo@msn.com>
2013-12-20 13:09:31 +0800

    32kbps quality is too bad

--- convert/convert.sh
+++ convert/convert.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-rm -f *.mp3 *.ogg
 for file in `ls ../clips/*.ogg`
 do
     fname=`basename $file | cut -d '.' -f1`
-    ffmpeg -loglevel panic -i ../clips/$fname.ogg -acodec libvorbis -ac 1 -ab 32k $fname.ogg
-    ffmpeg -loglevel panic -i ../clips/$fname.ogg -acodec libmp3lame -ac 1 -ab 32k $fname.mp3
+    rm -f $fname.mp3 $fname.ogg
+    ffmpeg -loglevel panic -i ../clips/$fname.ogg -acodec libvorbis -ac 1 -ab 48k $fname.ogg
+    ffmpeg -loglevel panic -i ../clips/$fname.ogg -acodec libmp3lame -ac 1 -ab 48k $fname.mp3
 done
