716545c7468e36e0a37a2d3a916f8f8b56487e34
CornGuo <cornguo@msn.com>
2013-12-27 11:22:28 +0800

    make script runnable under linux

--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 # check if git exists
-gitBin=`which git &>/dev/null`
-[ ! -z $gitBin ] || { echo "git not found"; exit 1; }
-echo "git found"
+gitBin=`which git`
+if [ ! -f $gitBin ];
+then
+    echo "git not found";
+    exit 1;
+else
+    echo "git found"
+fi
 # check if ffmpeg eixsts
 ffmpegBin=`which ffmpeg &>/dev/null`
-[ ! -z $ffmpegBin ] || { echo "ffmpeg not found"; exit 2; }
-echo "ffmpeg found"
+if [ -f $ffmpegBin ];
+then
+    echo "ffmpeg not found";
+    exit 2;
+else
+    echo "ffmpeg found"
+fi
 # get howler.js via submodule
 echo "getting howler.js..."
 $gitBin submodule init
