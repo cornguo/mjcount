d4767c35762d5bcbc6e6a0ef64ad1957d469d296
CornGuo <cornguo@msn.com>
2019-05-16 21:47:09 +0800

    remove old convert script

--- convert/convert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-for file in `ls ../clips/*.ogg`
-do
-    fname=`basename $file | cut -d '.' -f1`
-    rm -f $fname.mp3 $fname.ogg
-    ffmpeg -loglevel panic -i ../clips/$fname.ogg -acodec libvorbis -ac 1 -ab 48k $fname.ogg
-    ffmpeg -loglevel panic -i ../clips/$fname.ogg -acodec libmp3lame -ac 1 -ab 48k $fname.mp3
-done
